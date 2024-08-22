3dec12f7e4648d9f70ad8163204f608a3c9c4a1a
홍찬기 <chanki5451@douzone.com>
2024-08-22 11:44:17 +0900

    Initial .gitignore, gradle dependencies commit

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+	kotlin("jvm") version "1.9.24"
+	kotlin("plugin.spring") version "1.9.24"
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.breadkun"
+version = "0.0.1"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.liquibase:liquibase-core")
+//	spring-jdbc는 주로 동기식 JDBC 접근을 위해 사용되며, 리액티브 환경에서는 필요하지 않음
+//	리액티브 프로그래밍에서 JDBC 대신 R2DBC를 사용하므로, spring-jdbc가 불필요
+//	implementation("org.springframework:spring-jdbc")
+	implementation("org.springdoc:springdoc-openapi-webflux-ui:1.8.0")
+	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:r2dbc-postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/breadkun/backend/BreadkunBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.breadkun.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BreadkunBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BreadkunBackendApplication>(*args)
+}
