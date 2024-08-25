f8908893a29aa2e34da5aa4e562f81cf99192cf1
홍찬기 <chanki5451@douzone.com>
2024-08-25 23:13:36 +0900

    Gradle Enterprise 플러그인 설정 추가(setting.gradle.kts)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
 rootProject.name = "breadkun-backend"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.15.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
