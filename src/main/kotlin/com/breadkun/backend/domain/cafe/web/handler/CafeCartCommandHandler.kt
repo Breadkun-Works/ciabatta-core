6ac042dac06939986662eeca55c38e2f82ae70c9
홍찬기 <chanki5451@douzone.com>
2024-09-20 16:36:30 +0900

    CafeCart API 메타데이터(ID) 헤더로 이동

--- src/main/kotlin/com/breadkun/backend/domain/cafe/web/handler/CafeCartCommandHandler.kt
+++ src/main/kotlin/com/breadkun/backend/domain/cafe/web/handler/CafeCartCommandHandler.kt
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.awaitBody
+import org.springframework.web.reactive.function.server.bodyValueAndAwait
 
 @Component
-class CafeCartCommandHandler (
+class CafeCartCommandHandler(
     private val cafeCartCommandService: CafeCartCommandService
-){
+) {
     suspend fun createCafeCart(request: ServerRequest): ServerResponse {
+        val userUUID = request.headers().firstHeader("X-User-UUID")
+            ?: return ServerResponse.badRequest().bodyValueAndAwait("Missing X-User-UUID header")
         val cafeCartCreateDTO = request.awaitBody<CafeCartCreateDTO>()
-        val createdCart = cafeCartCommandService.createCafeCart(cafeCartCreateDTO)
+        val createdCart = cafeCartCommandService.createCafeCart(userUUID, cafeCartCreateDTO)
 
         return ResponseUtils.ok(createdCart)
     }
