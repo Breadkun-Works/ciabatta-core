9567b0726c9febda5a9a8f93b017110acf28c2a6
홍찬기 <coldair426@gmail.com>
2025-07-15 01:09:58 +0900

    refactor: 리포지토리 스캔 범위 명시(@EnableR2dbcRepositories,@EnableReactiveMongoRepositories) & 의존성 개선

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.postgresql:r2dbc-postgresql")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
--- src/main/kotlin/com/ciabatta/core/CiabattaCoreApplication.kt
+++ src/main/kotlin/com/ciabatta/core/CiabattaCoreApplication.kt
@@ -2,8 +2,16 @@ package com.ciabatta.core
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
+@EnableR2dbcRepositories(
+    basePackages = ["com.ciabatta.core.cafe.infrastructure.persistence.repository"],
+)
+@EnableReactiveMongoRepositories(
+    basePackages = ["com.ciabatta.core.dashboard.infrastructure.persistence.repository"],
+)
 class CiabattaCoreApplication
 
 fun main(args: Array<String>) {
